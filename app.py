445125399d962b82e050180d454440936083050b
Oksana911 <rosa111@mail.ru>
2022-06-21 21:43:32 +0700

    failed

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-# app.py
+# app
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
-from marshmallow import Schema, fields
+from models import Movie, Director, Genre
+from schemas import movie_schema, movies_schema
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -11,31 +12,85 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Movie(db.Model):
-    __tablename__ = 'movie'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
-    description = db.Column(db.String(255))
-    trailer = db.Column(db.String(255))
-    year = db.Column(db.Integer)
-    rating = db.Column(db.Float)
-    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
-    genre = db.relationship("Genre")
-    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
-    director = db.relationship("Director")
-
-class Director(db.Model):
-    __tablename__ = 'director'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+api = Api(app)
+movie_ns = api.namespace('movies')
 
 
-class Genre(db.Model):
-    __tablename__ = 'genre'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+@movie_ns.route('/')
+class MoviesViews(Resource):
+    def get(self):
+        all_movies = db.session.query(Movie).all()
+        return jsonify(movies_schema.dump(all_movies)), 200
 
-
-
-if __name__ == '__main__':
+#     def post(self):
+#         req = request.json
+#         new_movie = Movie(**req)
+#         with db.session.begin():
+#             db.session.add(new_movie)
+#         return "", 201
+#
+#
+# @movie_ns.route('/<int:uid>')
+# class MovieViews(Resource):
+#     def get(self, uid: int):
+#         try:
+#             movie = db.session.query(Movie).filter(Movie.id == uid).one()
+#             return movie_schema.dump(movie), 200
+#         except Exception as e:
+#             return str(e), 404
+#
+#     def put(self, uid: int):
+#         movie = db.session.query(Movie).get(uid)
+#         req_json = request.json
+#
+#         movie.title = req_json.get('title')
+#         movie.description = req_json.get('description')
+#         movie.trailer = req_json.get('trailer')
+#         movie.year = req_json.get('year')
+#         movie.rating = req_json.get('rating')
+#         movie.genre_id = req_json.get('genre_id')
+#         movie.genre = req_json.get('genre')
+#         movie.director_id = req_json.get('director_id')
+#         movie.director = req_json.get('director')
+#
+#         db.session.add(movie)
+#         db.session.commit()
+#         return "", 204
+#
+#     def patch(self, uid: int):
+#         movie = db.session.query(Movie).get(uid)
+#         req_json = request.json
+#
+#         if 'title' in req_json:
+#             movie.title = req_json.get('title')
+#         if 'description' in req_json:
+#             movie.description = req_json.get('description')
+#         if 'trailer' in req_json:
+#             movie.trailer = req_json.get('trailer')
+#         if 'year' in req_json:
+#             movie.year = req_json.get('year')
+#         if 'rating' in req_json:
+#             movie.rating = req_json.get('rating')
+#         if 'genre_id' in req_json:
+#             movie.genre_id = req_json.get('genre_id')
+#         if 'genre' in req_json:
+#             movie.genre = req_json.get('genre')
+#         if 'director_id' in req_json:
+#             movie.director_id = req_json.get('director_id')
+#         if 'director' in req_json:
+#             movie.director = req_json.get('director')
+#
+#         db.session.add(movie)
+#         db.session.commit()
+#
+#         return "", 204
+#
+#     def delete(self, uid: int):
+#         book = db.session.query(Movie).get(uid)
+#         db.session.delete(book)
+#         db.session.commit()
+#         return "", 204
+#
+#
+if __name__ == "__main__":
     app.run(debug=True)
